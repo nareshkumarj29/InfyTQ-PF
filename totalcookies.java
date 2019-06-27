e73d938d17fbd8e31a079bde593616e6538facc7
nareshkumarj29 <52150586+nareshkumarj29@users.noreply.github.com>
2019-06-27 12:24:58 +0530

    Create totalcookies.java
    java program for calculating total cookies for airline passengers

---
+++ totalcookies.java
@@ -0,0 +1,15 @@
+
+
+noOfFlightsTakeOff=100
+noOfFlightsLanded=110
+seatingCapacityTakeOff=150
+seatingCapacityLanded=185
+totalCookies=0
+
+
+totalCookies=(((noOfFlightsTakeOff*seatingCapacityTakeOff)+(noOfFlightsLanded*seatingCapacityLanded/2))*2)
+
+
+
+
+console.log("totalCookies="+totalCookies)
